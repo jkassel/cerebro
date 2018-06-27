f0dafe1627445e5f83305a69a3855376edd518fb
Jeff Kassel <jkassel@gmail.com>
2018-06-27 15:10:18 -0400

    setting production db

--- project/server/config.py
+++ project/server/config.py
@@ -39,5 +39,5 @@ class ProductionConfig(BaseConfig):
     """Production configuration."""
     SECRET_KEY = '7d84ed5818d603c468af50dda9289f0f2af0c70c0ea753c1041a6635aef1dcb9'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/example'
+    SQLALCHEMY_DATABASE_URI = 'postgres://qhngbffpbuflsp:20614672c5f4d50fa1b731a6bed4e29f6e05f16e235129535f95ebc1a97d3363@ec2-184-73-240-228.compute-1.amazonaws.com:5432/da5slfsl8j6sf3'
     DEBUG_TB_ENABLED = False
